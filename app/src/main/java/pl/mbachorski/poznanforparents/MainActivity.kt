d28346664700c890205746b63c65446fb5191d13
Marek Bachorski <m.bachorski@gmail.com>
2018-12-30 12:54:23 +0100

    Adds bottom menu

--- app/src/main/java/pl/mbachorski/poznanforparents/MainActivity.kt
+++ app/src/main/java/pl/mbachorski/poznanforparents/MainActivity.kt
@@ -2,7 +2,6 @@ package pl.mbachorski.poznanforparents
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -17,11 +16,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(toolbar)
-
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show()
-    }
 
     val toggle = ActionBarDrawerToggle(
       this,
@@ -50,29 +44,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     return true
   }
 
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    return when (item.itemId) {
-      R.id.action_settings -> true
-      else -> super.onOptionsItemSelected(item)
-    }
-  }
-
   override fun onNavigationItemSelected(item: MenuItem): Boolean {
     // Handle navigation view item clicks here.
     when (item.itemId) {
       R.id.nav_camera -> {
         // Handle the camera action
       }
-      R.id.nav_gallery -> {
-
-      }
       R.id.nav_slideshow -> {
-
-      }
-      R.id.nav_manage -> {
 
       }
       R.id.nav_share -> {
